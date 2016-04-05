9063fbe55d76c2eb807a1f2d36c31a73cf4e0db3
Jacob Riddle <ironlenny@gmail.com>
2016-04-04 19:17:26 -0700

    Fix bug in cli.py where cli.py closes when no arguments are given.

--- cli.py
+++ cli.py
@@ -11,19 +11,19 @@ def cli(filename='infodump.conf'):
     config.read_file(open(filename))
   except configparser.MissingSectionHeaderError as err:
     print(err)
-    
-  sys.exit()
-  
+      
   controller = control.Controller(config)
   parser = argparse.ArgumentParser()
   parser.add_argument('-i', '--import', nargs='?', metavar='filename,tag,tag,...', help='import the contents of a file as a note and tag it with the given tags')
-  args = vars(parser.parse_args(sys.argv[1:]))
-  argKeys = list(args.keys())
   
-  for case in switch.switch(argKeys):
-    if case(['import']):
-      argsList = args['import'].split(',')
-      note = open(argsList[0], r)
-      controller.save_note(note.read(), argsList[1:])
-      
+  if len(sys.argv) != 1:
+    args = vars(parser.parse_args(sys.argv[1:]))
+    argKeys = list(args.keys())
+  
+    for case in switch.switch(argKeys):
+      if case(['import']):
+        argsList = args['import'].split(',')
+        note = open(argsList[0], r)
+        controller.save_note(note.read(), argsList[1:])
+        
   return controller
